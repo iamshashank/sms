c4108c1f3529c60f467efc03a70dedc0142d0616
code4fun0x1 <shashank0x1@gmail.com>
2017-04-16 15:43:12 +0530

    fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const port = process.env.PORT|3000;
+const port = process.env.PORT||3000;
 var accountSid = 'AC2ef950645e3538bc368e250c75535690';
 var authToken = '50dc8a6c16592e48c1c22d33c4268acc';
 
